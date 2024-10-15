5b4f852f6af353eff07f4dc6d28fadbb22649f09
ebertmillar <ebertmillar@gmail.com>
2024-10-15 11:05:52 +0200

    varaibles de entorno configuradas

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 
 import 'package:cinemapedia_app/config/router/app_router.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 import 'package:cinemapedia_app/config/themes/app_theme.dart';
 
-void main() {
+Future main() async {
+  await dotenv.load(fileName: ".env");
   runApp(const MainApp());
 }
 
