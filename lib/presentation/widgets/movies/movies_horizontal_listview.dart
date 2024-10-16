4204376607177753280a19173393231826fbcc6a
ebertmillar <ebertmillar@gmail.com>
2024-10-16 11:31:36 +0200

    se agrego dependencia intl, para dar formato de numeros cortos, se aplico formato a likes

--- lib/presentation/widgets/movies/movies_horizontal_listview.dart
+++ lib/presentation/widgets/movies/movies_horizontal_listview.dart
@@ -1,5 +1,6 @@
 import 'package:animate_do/animate_do.dart';
 import 'package:cinemapedia_app/config/domain/entities/movie.dart';
+import 'package:cinemapedia_app/config/helpers/human_formats.dart';
 import 'package:flutter/material.dart';
 
 class MoviesHorizontalListview extends StatelessWidget {
@@ -90,17 +91,21 @@ class _Slide extends StatelessWidget {
           ),
 
           //Rating - star
-          Row(
-            children: [
-              Icon ( Icons.star_half_outlined, color: Colors.yellow.shade900),
-              const SizedBox(width: 3),
-              Text(
-                '${movie.voteAverage}', 
-                style: titleStyle.bodyMedium?.copyWith(color: Colors.yellow.shade900),
-              ),
-              const SizedBox(width: 10),
-              Text('${movie.popularity}', style: titleStyle.bodySmall,)
-            ],
+          SizedBox(
+            width: 150,
+            child: Row(
+              children: [
+                Icon ( Icons.star_half_outlined, color: Colors.yellow.shade900),
+                const SizedBox(width: 3),
+                Text(
+                  '${movie.voteAverage}', 
+                  style: titleStyle.bodyMedium?.copyWith(color: Colors.yellow.shade900),
+                ),
+                const Spacer(),
+                //Text('${movie.popularity}', style: titleStyle.bodySmall,)
+                Text(HumanFormats.number(movie.popularity), style: titleStyle.bodyMedium,)
+              ],
+            ),
           ),
         ],
       ),
