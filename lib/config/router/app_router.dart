ed5e4da302a4e0918b3fb1f84055805d987f26a4
ebertmillar <ebertmillar@gmail.com>
2024-10-22 10:10:13 +0200

    se implemento navegacion, custom_bottom_navigator

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,27 +1,65 @@
 
 import 'package:cinemapedia_app/presentation/screens/screens.dart';
+import 'package:cinemapedia_app/presentation/views/home_views/favorites_views.dart';
+import 'package:cinemapedia_app/presentation/views/views.dart';
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 final appRouter = GoRouter(
   initialLocation: '/',
   routes: [
 
-    GoRoute(
-      path: '/',
-      name: HomeScreen.name,
-      builder: (context, state) => const HomeScreen(),
+    ShellRoute(
+      builder: (context, state, child) {
+        return HomeScreen(childView: child);
+
+      },
       routes: [
 
         GoRoute(
-          path: 'movie/:id',
-          name: MovieScreen.name,
+          path: '/',
           builder: (context, state) {
-            final movieId = state.pathParameters['id'] ?? 'no-id';
-            return MovieScreen(movieId: movieId);
-          }
+            return const HomeView();
+          },
+          routes: [
+            GoRoute(
+              path: 'movie/:id',
+              name: MovieScreen.name,
+              builder: (context, state) {
+                final movieId = state.pathParameters['id'] ?? 'no-id';
+                return MovieScreen(movieId: movieId);
+              }
+            )
+          ]
         ),
+
+        GoRoute(
+          path: '/favorites',
+          builder: (context, state) {
+            return const FavoritesViews();
+          },
+        )
       ]
-    ),
+    )
+
+
+    //Rutas padre- hija
+    // GoRoute(
+    //   path: '/',
+    //   name: HomeScreen.name,
+    //   builder: (context, state) => const HomeScreen( childView: FavoritesViews()),
+    //   routes: [
+
+    //     GoRoute(
+    //       path: 'movie/:id',
+    //       name: MovieScreen.name,
+    //       builder: (context, state) {
+    //         final movieId = state.pathParameters['id'] ?? 'no-id';
+    //         return MovieScreen(movieId: movieId);
+    //       }
+    //     ),
+    //   ]
+    // ),
 
     
     
--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,117 +1,23 @@
-import 'package:cinemapedia_app/presentation/providers/providers.dart';
+import 'package:cinemapedia_app/presentation/views/home_views/home_view.dart';
 import 'package:cinemapedia_app/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class HomeScreen extends StatelessWidget {
   static const name = 'home-screen';
 
-  const HomeScreen({super.key});
+  final Widget childView;
+
+  const HomeScreen({
+    super.key, 
+    required this.childView
+  });
+  
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: _HomeView(),
-      bottomNavigationBar: CustomBottomNavigation(),
+    return Scaffold(
+      body: childView,
+      bottomNavigationBar: const CustomBottomNavigation(),
     );
   }
 }
 
-class _HomeView extends ConsumerStatefulWidget {
-  const _HomeView();
-
-  @override
-  _HomeViewState createState() => _HomeViewState();
-}
-
-class _HomeViewState extends ConsumerState<_HomeView> {
-  @override
-  void initState() {
-    super.initState();
-    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
-    ref.read(popularMoviesProvider.notifier).loadNextPage();
-    ref.read(topRatingMoviesProvider.notifier).loadNextPage();
-    ref.read(upComingMoviesProvider.notifier).loadNextPage();
-
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    final initialLoading = ref.watch(initialLoadingProvider);
-    if(initialLoading == true) return  const FullScreenLoader();
-
-    final slideShowMovies = ref.watch(moviesSlideShowProvider);
-    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
-    final popularMovies = ref.watch(popularMoviesProvider);
-    final topRatingMovies = ref.watch(topRatingMoviesProvider);
-    final upComingMovies = ref.watch(upComingMoviesProvider);
-
-    return CustomScrollView(
-      slivers: [
-        const SliverAppBar(
-          floating: true,
-          flexibleSpace: FlexibleSpaceBar(
-            title: CustomAppbar(),
-            titlePadding: EdgeInsets.symmetric(
-                horizontal: 0), // Añade espaciado horizontal
-          ),
-        ),
-        SliverList(
-            delegate: SliverChildBuilderDelegate((context, index) {
-          return Column(
-            children: [
-              //const CustomAppbar(),
-              MoviesSlideshow(movies: slideShowMovies),
-              MoviesHorizontalListview(
-                movies: nowPlayingMovies,
-                title: 'En cines',
-                subTitle: 'Lunes 20',
-                loadNextPage: () =>
-                    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage(),
-              ),
-              MoviesHorizontalListview(
-                movies: upComingMovies,
-                title: 'Proximamente',
-                //subTitle: 'En este mes',
-                loadNextPage: () =>
-                    ref.read(upComingMoviesProvider.notifier).loadNextPage(),
-              ),
-              MoviesHorizontalListview(
-                movies: popularMovies,
-                title: 'Populares',
-                subTitle: 'Esta semana',
-                loadNextPage: () =>
-                    ref.read(popularMoviesProvider.notifier).loadNextPage(),
-              ),
-              MoviesHorizontalListview(
-                movies: topRatingMovies,
-                title: 'Mas recomendadas',
-                subTitle: 'Este mes',
-                loadNextPage: () =>
-                    ref.read(topRatingMoviesProvider.notifier).loadNextPage(),
-              ),
-              const SizedBox(height: 20),
-
-              /*Expanded(
-            child: ListView.builder(
-            itemCount: nowPlayingMovies.length ,
-            itemBuilder: (context, index) {
-              final movie = nowPlayingMovies[index];
-              return ListTile(
-                title: Text(movie.title),
-              );
-              },
-            ),
-          ),*/
-            ],
-          );
-        }, childCount: 1)),
-      ],
-    );
-  }
-}
